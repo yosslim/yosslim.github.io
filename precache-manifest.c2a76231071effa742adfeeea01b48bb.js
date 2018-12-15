1e18e286f68e5b6a7ed7ff10ba45e765a5d6e8d8
Yorceli <ylima.esp@gmail.com>
2018-12-14 16:06:27 -0800

    adding files

---
+++ precache-manifest.c2a76231071effa742adfeeea01b48bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b41bd25a3be30edb74c3",
+    "url": "/static/js/main.b41bd25a.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "b41bd25a3be30edb74c3",
+    "url": "/static/css/main.7ae15b35.chunk.css"
+  },
+  {
+    "revision": "2106021197985c929297a859f81dc08b",
+    "url": "/index.html"
+  }
+];
